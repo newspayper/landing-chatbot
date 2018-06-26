f3f286c89cd2edd47aeb2b04a9436c4e192add7e
ElBulgaro <34547278+ElBulgaro@users.noreply.github.com>
2018-06-26 15:39:25 +0200

    Customer Chat + Layout OK / Mockup NOK

--- public/js/fb.js
+++ public/js/fb.js
@@ -1,3 +1,5 @@
+// Send to Messenger
+
   window.fbAsyncInit = function() {
     FB.init({
       appId            : '1678638095724206',
@@ -14,4 +16,33 @@
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
+// Customer Chat Plugin
+
+  window.fbMessengerPlugins = window.fbMessengerPlugins || {
+    init: function () {
+      FB.init({
+        appId            : '1678638095724206',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v2.10'
+      });
+    }, callable: []
+  };
+
+  window.fbAsyncInit = window.fbAsyncInit || function () {
+    window.fbMessengerPlugins.callable.forEach(function (item) { item(); });
+    window.fbMessengerPlugins.init();
+  };
+
+  setTimeout(function () {
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, 0);
     
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
-
-          $('.event.example .image')
-            .dimmer({
-              on: 'hover'
-            })
-          ;
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
