46d4b7332133bed201fd2b47dc4d0cde6a4955fb
ElBulgaro <34547278+ElBulgaro@users.noreply.github.com>
2018-07-02 18:37:47 +0200

    Features v1 - FB SDK OK
    Added stackable row of features using Messenger emoji-type images + Semantic Header
    Added French version of Facebook JS SDK (Send to Messenger  + Customer Chat Plugin)
    Added Personnalized Greetings + Color on Customer Chat Plugin
    
    NOK :
    - Device Block not done
    - Padding / Margin / Width on "Essayez-moi" and header/features

--- public/js/fb.js
+++ public/js/fb.js
@@ -1,4 +1,8 @@
-// Send to Messenger
+  
+// Facebook SDK for JavaScript
+// Source : https://developers.facebook.com/docs/javascript/quickstart
+// Localization : fr_FR
+// App-ID : 1678638095724206
 
   window.fbAsyncInit = function() {
     FB.init({
@@ -13,36 +17,6 @@
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     js.src = "https://connect.facebook.net/fr_FR/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-
-// Customer Chat Plugin
-
-  window.fbMessengerPlugins = window.fbMessengerPlugins || {
-    init: function () {
-      FB.init({
-        appId            : '1678638095724206',
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v2.10'
-      });
-    }, callable: []
-  };
-
-  window.fbAsyncInit = window.fbAsyncInit || function () {
-    window.fbMessengerPlugins.callable.forEach(function (item) { item(); });
-    window.fbMessengerPlugins.init();
-  };
-
-  setTimeout(function () {
-    (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }, 0);
-    
